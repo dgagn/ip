09740150db74d0fd033a95e880508c035498179b
Dany Gagnon <danygagnon@Danys-MacBook-Air.local>
2022-03-13 18:42:45 -0400

    feat: add post

--- database/migrations/2022_03_13_183826_create_posts_table.php
+++ database/migrations/2022_03_13_183826_create_posts_table.php
@@ -12,8 +12,13 @@ return new class () extends Migration {
      */
     public function up()
     {
-        Schema::create('ip_addresses', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
+
+            $table->ipAddress('ip');
+            $table->text('body');
+            $table->foreignId('user_id')->nullable()->constrained()->cascadeOnDelete();
+
             $table->timestamps();
         });
     }
@@ -25,6 +30,6 @@ return new class () extends Migration {
      */
     public function down()
     {
-        Schema::dropIfExists('ip_addresses');
+        Schema::dropIfExists('posts');
     }
 };
